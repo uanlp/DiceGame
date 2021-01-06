ad7fdc595e1de09b6ffae5ed837453756fc661e2
uanlp <63435119+uanlp@users.noreply.github.com>
2021-01-06 15:06:51 -0600

    Adding Dice game files

---
+++ index.js
@@ -0,0 +1,26 @@
+//Randomize number from 1 - 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 +".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 +".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+//Decide which player wins
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player1 wins!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player2 wins"
+}else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
